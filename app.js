0d7eeb5a7450a648303c5e9537656187a60b0159
Zach Hamby <zhamby1@ivytech.edu>
2023-12-05 14:31:44 -0500

    added-update-details

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const Song = require("./models/song");
 var cors = require('cors')
+// const bodyParser = require('body-parser')
+const jwt = require('jwt-simple')
+const User = require("./models/users")
 
 const app = express();
 app.use(cors())
@@ -9,6 +12,31 @@ app.use(cors())
 app.use(express.json());
 
 const router = express.Router();
+const secret = "supersecret"
+
+//creating a new user
+router.post("/user", async(req,res) =>{
+   if(!req.body.username || !req.body.password){
+      res.status(400).json({error: "Missing username or password"})
+   }
+
+   const newUser = await new User({
+      username: req.body.username,
+      password: req.body.password,
+      status: req.body.status
+
+   })
+
+   try{
+      await newUser.save()
+      console.log(newUser)
+      res.sendStatus(201) //created
+   }
+   catch(err){
+      res.status(400).send(err)
+   }
+
+})
 
 // Get list of all songs in the database
 router.get("/songs", async(req,res) =>{
@@ -19,6 +47,7 @@ router.get("/songs", async(req,res) =>{
    }
    catch (err){
       console.log(err)
+      res.status(400).send(err)
    }
 
 })
@@ -66,6 +95,23 @@ router.put("/songs/:id", async(req,res) =>{
    catch(err){
       res.status(400).send(err)
    }
+})
+
+router.delete("/songs/:id", async(req,res) =>{
+   //method or function in mongoose/mongo to delete a single instance of a song or object 
+   try{
+      const song = await Song.findById(req.params.id)
+      console.log(song)
+      await Song.deleteOne({ _id: song._id })
+      res.sendStatus(204)
+   }
+
+   catch(err){
+      res.status(400).send(err)
+   }
+   
+
+
 })
 
 
